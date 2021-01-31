56c79502f21b05196e0bf6105a07316abb9b5a9d
Juanjo96Dev <juanjosealosnero@gmail.com>
2021-01-31 22:47:33 +0100

    bluetooh devices

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
+
 import 'package:scooter_info/views/listDevices.view.dart';
 
-void main() {
-  runApp(
-    MyApp()
-  );
-}
-
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  MyApp({Key key})
-      : super(key: key);
-
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ListDevices(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: MyHomePage(),
+  );
 }
